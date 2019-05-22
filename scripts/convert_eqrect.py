fix error due to image rotation

scripts/convert_eqrect.py
@@ -12,7 +12,7 @@ from cv_bridge import CvBridge, CvBridgeError
 
 #from multiprocessing import Pool
 
-def read_params_camodocal(filename):
+def read_ucm_params_camodocal(filename):
     with open(filename, 'r') as stream:
         skip_lines = 2
         for i in range(skip_lines):
@@ -32,6 +32,23 @@ def read_params_camodocal(filename):
     xi = np.array([data['mirror_parameters']['xi']])
     return xi, K, D
 
+def read_ucm_params_kalibr(filename):
+    with open(filename, 'r') as stream:
+        try:
+            data = yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+    dp = data['cam0']['distortion_coeffs']
+    D = np.array(dp)
+    pp = data['cam0']['intrinsics']
+    K = np.eye(3)
+    K[0][0] = pp[1]
+    K[1][1] = pp[2]
+    K[0][2] = pp[3]
+    K[1][2] = pp[4]
+    xi = np.array(pp[:1])
+    return xi, K, D
+
 def initRectifyMap(K, D, xi):
     flags = cv2.omnidir.RECTIFY_LONGLATI
     new_size = (640, 640)
@@ -40,15 +57,18 @@ def initRectifyMap(K, D, xi):
     nw = new_size[1]
     Knew[0][0] = nw / 3.141592
     Knew[1][1] = nh / 3.141592
-    R = np.eye(3)
+    theta = np.radians(90)
+    c, s = np.cos(theta), np.sin(theta)
+    R = np.array(((c,-s, 0), (s, c, 0), (0, 0, 1)))
     map1, map2 = cv2.omnidir.initUndistortRectifyMap(K, D, xi, R, Knew, new_size, cv2.CV_16SC2, flags)
     return map1, map2
 
 def equirectangular_projection(img, map1, map2):
+    undistorted_img = cv2.remap(img, map1, map2, cv2.INTER_LINEAR, cv2.BORDER_CONSTANT)
+    return undistorted_img
+
+def rotate_cw_90(img):
     img_t = cv2.transpose(img)
-    img_f = cv2.flip(img_t, flipCode=1)
-    undistorted_img = cv2.remap(img_f, map1, map2, cv2.INTER_LINEAR, cv2.BORDER_CONSTANT)
-    img_t = cv2.transpose(undistorted_img)
     img_f = cv2.flip(img_t, flipCode=0)
     return img_f
 
@@ -65,14 +85,8 @@ def callback(data):
     front_eqimg = equirectangular_projection(front_img, map1_f, map2_f)
     back_eqimg  = equirectangular_projection(back_img,  map1_b, map2_b)
 
-    #pool = Pool(processes=2)
-    #front_eqimg = pool.apply_async(equirectangular_projection, (front_img, map1_f, map2_f))
-    #back_eqimg  = pool.apply_async(equirectangular_projection, (back_img,  map1_b, map2_b))
-    #pool.close()
-    #pool.join()
-    #both_eqimg = np.concatenate((front_eqimg.get(), back_eqimg.get()), axis=1)
+    both_eqimg = np.concatenate((rotate_cw_90(front_eqimg), rotate_cw_90(back_eqimg)), axis=1)
 
-    both_eqimg = np.concatenate((front_eqimg, back_eqimg), axis=1)
     panorama = np.roll(both_eqimg, 320, axis=1)
 
     image_message = bridge.cv2_to_imgmsg(panorama, encoding="bgr8")
@@ -87,7 +101,7 @@ if __name__ == '__main__':
 
     if rospy.has_param('~front_calib'):
         params_file = rospy.get_param('~front_calib')
-        xi_f, K_f, D_f = read_params_camodocal(params_file)
+        xi_f, K_f, D_f = read_ucm_params_camodocal(params_file)
         map1_f, map2_f = initRectifyMap(K_f, D_f, xi_f)
     else:
         print("Front camera calibration file is missing.")
@@ -95,7 +109,7 @@ if __name__ == '__main__':
 
     if rospy.has_param('~back_calib'):
         params_file = rospy.get_param('~back_calib')
-        xi_b, K_b, D_b = read_params_camodocal(params_file)
+        xi_b, K_b, D_b = read_ucm_params_camodocal(params_file)
         map1_b, map2_b = initRectifyMap(K_b, D_b, xi_b)
     else:
         print("Back camera calibration file is missing.")