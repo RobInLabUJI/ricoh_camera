355d2b79c5b3989a7a92f1d27936b3bbd765d8ca
Enric Cervera <ecervera@uji.es>
2019-05-23 11:50:23 +0200

    add calibration with apriltags

---
+++ calibration/kalibr/apriltags/run_kalibr_calibrate_cameras_in_docker.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p $1
+
+docker run --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v `pwd`:/data:rw ros-kalibr \
+  bash -c "cd /data && \
+           kalibr_calibrate_cameras --target april_6x4.yaml --bag $3_camera.bag \
+             --models $1 --topics /$2/image_$3_raw --show-extraction && \
+           chown $UID.${GROUPS[0]} camchain-*.yaml results-*.txt report-*.pdf"
+
+mv camchain-*.yaml results-*.txt report-*.pdf $1
+
