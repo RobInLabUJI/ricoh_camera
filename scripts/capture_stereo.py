4cce84bb990c88c63a7aff068f74854d49d01233
Enric Cervera <ecervera@uji.es>
2019-07-03 14:55:49 +0200

    rewrite and document stereo capture

--- scripts/capture_stereo.py
+++ scripts/capture_stereo.py
@@ -6,103 +6,37 @@ import time
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
-from VideoCaptureAsync import VideoCaptureAsync
 
-import numpy as np
 import cv2
 
-from threading import Thread,Semaphore
-
-class Barrier:
-    def __init__(self, n):
-        self.n = n
-        self.count = 0
-        self.mutex = Semaphore(1)
-        self.barrier = Semaphore(0)
-
-    def wait(self):
-        self.mutex.acquire()
-        self.count = self.count + 1
-        self.mutex.release()
-        if self.count == self.n: self.barrier.release()
-        self.barrier.acquire()
-        self.barrier.release()
-
 def talker():
-    global cap1
-    global cap2
-    global split
+    global cap
     global frequency
-    global video_width
-    global display
-
-    image_size = video_width / 2
+    global video_resolution
+    
+    image_size = video_resolution / 2
 
     bridge = CvBridge()
-    if split:
-        pub_f1 = rospy.Publisher('cam_bottom/image_front_raw', Image, queue_size=10)
-        pub_b1 = rospy.Publisher('cam_bottom/image_back_raw', Image, queue_size=10)
-        pub_f2 = rospy.Publisher('cam_top/image_front_raw', Image, queue_size=10)
-        pub_b2 = rospy.Publisher('cam_top/image_back_raw', Image, queue_size=10)
-    else:
-        pub1 = rospy.Publisher('cam_bottom/image_raw', Image, queue_size=10)
-        pub2 = rospy.Publisher('cam_top/image_raw', Image, queue_size=10)
-        
+    pub1 = rospy.Publisher('cam_bottom/image_raw', Image, queue_size=10)
+    pub2 = rospy.Publisher('cam_top/image_raw',    Image, queue_size=10)
     rate = rospy.Rate(frequency)
     while not rospy.is_shutdown():
-        ret, frame1, stamp1 = cap1.read()
-        ret, frame2, stamp2 = cap2.read()
-        
-        front = frame1[0:image_size, 0:image_size]
-        front = cv2.transpose(front)
-        front = cv2.flip(front, flipCode=1)
-        back  = frame1[0:image_size, image_size:2*image_size]
-        back = cv2.transpose(back)
-        back = cv2.flip(back, flipCode=0)
-        output = np.concatenate( (front, back), axis=1 )
-        if display:
-            cv2.imshow('/dev/video%d'%video_input_1, output)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        if split:
-            image_f_message = bridge.cv2_to_imgmsg(front, encoding="bgr8")
-            image_b_message = bridge.cv2_to_imgmsg(back,  encoding="bgr8")
-            image_f_message.header.stamp = stamp1
-            image_b_message.header.stamp = stamp1
-            pub_f1.publish(image_f_message)
-            pub_b1.publish(image_b_message)
-        else:
-            image_message = bridge.cv2_to_imgmsg(output, encoding="bgr8")
-            image_message.header.stamp = stamp1
-            pub1.publish(image_message)
-            
-        front = frame2[0:image_size, 0:image_size]
-        front = cv2.transpose(front)
-        front = cv2.flip(front, flipCode=1)
-        back  = frame2[0:image_size, image_size:2*image_size]
-        back = cv2.transpose(back)
-        back = cv2.flip(back, flipCode=0)
-        output = np.concatenate( (front, back), axis=1 )
-        if display:
-            cv2.imshow('/dev/video%d'%video_input_2, output)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        if split:
-            image_f_message = bridge.cv2_to_imgmsg(front, encoding="bgr8")
-            image_b_message = bridge.cv2_to_imgmsg(back,  encoding="bgr8")
-            image_f_message.header.stamp = stamp2
-            image_b_message.header.stamp = stamp2
-            pub_f2.publish(image_f_message)
-            pub_b2.publish(image_b_message)
-        else:
-            image_message = bridge.cv2_to_imgmsg(output, encoding="bgr8")
-            image_message.header.stamp = stamp2
-            pub2.publish(image_message)
-            
+        cap1.grab()
+        stamp1 = rospy.Time.from_sec(time.time())
+        cap2.grab()
+        stamp2 = rospy.Time.from_sec(time.time())
+        ret, frame1 = cap1.retrieve()
+        ret, frame2 = cap2.retrieve()
+        image1_message = bridge.cv2_to_imgmsg(frame1[0:image_size,:], encoding="bgr8")
+        image1_message.header.stamp = stamp1
+        pub1.publish(image1_message)
+        image2_message = bridge.cv2_to_imgmsg(frame2[0:image_size,:], encoding="bgr8")
+        image2_message.header.stamp = stamp2
+        pub2.publish(image2_message)
         rate.sleep()
 
 if __name__ == '__main__':
-    rospy.init_node('capture', anonymous=True)
+    rospy.init_node('capture_stereo', anonymous=True)
 
     if rospy.has_param('~video_input_bottom'):
         video_input_1 = rospy.get_param('~video_input_bottom')
@@ -114,51 +48,37 @@ if __name__ == '__main__':
     else:
         video_input_2 = 1
 
-    supported_widths = [1280, 1920]
-    if rospy.has_param('~width'):
-        video_width = rospy.get_param('~width')
-        if not video_width in supported_widths:
-            print("Unsupported width %d: valid options %s" % (video_width, supported_widths))
+    supported_resolutions = [1280, 1920]
+    if rospy.has_param('~resolution'):
+        video_resolution = rospy.get_param('~resolution')
+        if not video_resolution in supported_resolutions:
+            print("Unsupported resolution %d: valid options %s" % (video_resolution, supported_resolutions))
+            video_resolution = supported_resolutions[0]
+            print("Using default: %d" % video_resolution)
     else:
-        video_width = supported_widths[0]
-
-    supported_heights = [720, 1080]
-    if rospy.has_param('~height'):
-        video_height = rospy.get_param('~height')
-        if not video_height in supported_heights:
-            print("Unsupported height %d: valid options %s" % (video_height, supported_heights))
-    else:
-        video_height = supported_heights[0]
-
-    if rospy.has_param('~split'):
-        split = rospy.get_param('~split')
-    else:
-        split = False
-
-    if rospy.has_param('~display'):
-        display = rospy.get_param('~display')
-    else:
-        display = False
+        video_resolution = supported_resolutions[0]
 
     if rospy.has_param('~frequency'):
         frequency = rospy.get_param('~frequency')
-        if frequency > 30:
+        if frequency < 1 or frequency > 30:
+            print("Unsupported frequency %d: valid interval [1, 30]" % frequency)
             frequency = 30
+            print("Using default: %d" % frequency)
     else:
-        frequency = 15
+        frequency = 30
 
-    b = Barrier(2)
-    cap1 = VideoCaptureAsync(video_input_1, video_width, video_height, b)
-    cap2 = VideoCaptureAsync(video_input_2, video_width, video_height, b)
-    #cap1.set(cv2.CAP_PROP_FRAME_WIDTH, video_width)
-    #cap2.set(cv2.CAP_PROP_FRAME_HEIGHT,video_height)
-    cap1.start()
-    cap2.start()
+    video_height = {1280: 720, 1920: 1080}
+
+    cap1 = cv2.VideoCapture(video_input_1)
+    cap2 = cv2.VideoCapture(video_input_2)
+    cap1.set(cv2.CAP_PROP_FRAME_WIDTH, video_resolution)
+    cap1.set(cv2.CAP_PROP_FRAME_HEIGHT,video_height[video_resolution])
+    cap2.set(cv2.CAP_PROP_FRAME_WIDTH, video_resolution)
+    cap2.set(cv2.CAP_PROP_FRAME_HEIGHT,video_height[video_resolution])
     try:
         talker()
     except rospy.ROSInterruptException:
         pass
-    cap1.stop()
-    cap2.stop()
+    cap1.release()
+    cap2.release()
     cv2.destroyAllWindows()
-
